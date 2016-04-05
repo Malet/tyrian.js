0534a76d4a12ee32adc838293bdc23ce81df40f6
Michael Malet <michael@rubypx.com>
2016-04-05 21:28:28 +0100

    Resize to match tyrian resolution (320x240!)

--- main.jsx
+++ main.jsx
@@ -3,6 +3,6 @@ let ReactDOM = require('react-dom');
 let SceneComponent = require('./components/scene_component.jsx');
 
 ReactDOM.render(
-  <SceneComponent width="640" height="480"/>,
+  <SceneComponent width="320" height="240"/>,
   document.getElementById('main')
 );
