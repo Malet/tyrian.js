5885ea88eb28a4050340cfc3fbe82a508b297fc9
Michael Malet <michael@rubypx.com>
2016-04-05 10:26:41 +0100

    Performance testing of DOM rendering

--- components/bullet_component.jsx
+++ components/bullet_component.jsx
@@ -1,13 +1,16 @@
 let React = require('react');
 let classnames = require('classnames');
 
-module.exports = class ShipComponent extends React.Component {
+module.exports = class BulletComponent extends React.Component {
   constructor(props) {
     super(props);
     this.bulletWidth = 6;
   }
 
   render() {
-    return <div className="bullet" style={{left: this.props.x - (this.bulletWidth / 2)}}></div>;
+    return <div className="bullet" style={{
+      left: this.props.x - (this.bulletWidth / 2),
+      bottom: this.props.y
+    }}></div>;
   }
 };
--- components/scene_component.jsx
+++ components/scene_component.jsx
@@ -11,14 +11,23 @@ module.exports = class SceneComponent extends React.Component {
     super(props);
 
     this.shipWidth = 18;
+    this.shipHeight = 25;
     this.sizing = {
-      width: `${this.props.width}px`,
-      height: `${this.props.height}px`
+      width: this.props.width,
+      height: this.props.height
     };
+    this.ticker = setInterval(
+      _ => {
+        this._fireBullet();
+        this._propelBullets();
+      },
+      1000 / 60
+    )
 
     this.state = {
       shipPositionX: (this.props.width / 2) - (this.shipWidth / 2),
-      bullets: []
+      bullets: [],
+      bulletCounter: 0
     };
   }
 
@@ -28,27 +37,55 @@ module.exports = class SceneComponent extends React.Component {
 
     $(document).mousemove(mouseEvent => {
       let x = Math.round(mouseEvent.pageX - offset.left);
-      let shipPosition = Math.max(0, Math.min(x, this.props.width - this.shipWidth));
+      let shipPosition = Math.max(
+        0,
+        Math.min(x, this.props.width - this.shipWidth)
+      );
       this.setState({ shipPositionX: shipPosition });
     });
 
-    $(document).click(clickEvent => {
-      // Create bullet
-      let bullet = {
-        x: this.state.shipPositionX + (this.shipWidth / 2)
-      };
-      this.setState({ bullets: this.state.bullets.concat(bullet) });
-    });
+    $(document).click(this._fireBullet.bind(this));
   }
 
   render() {
     return <div style={this.sizing} className={classnames({scene: true})}>
-      {this.state.bullets.map(bullet => <BulletComponent x={bullet.x}/>)}
+      {this.state.bullets.map(bullet => {
+        return <BulletComponent x={bullet.x} y={bullet.y} key={bullet.key}/>
+      })}
       <ShipComponent x={this.state.shipPositionX}/>
+      <div className="text">
+        Bullets fired: <strong>{this.state.bulletCounter}</strong>
+      </div>
+      <div className="text">
+        Elements on screen: <strong>{this.state.bullets.length}</strong>
+      </div>
     </div>;
   }
 
   _moveShip(by) {
     this.setState({shipPositionX: this.state.shipPositionX + (by * 5)});
+  }
+
+  _fireBullet(clickEvent) {
+    let key = this.state.bulletCounter;
+    let bullet = {
+      key: key,
+      x: this.state.shipPositionX + (this.shipWidth / 2),
+      y: this.shipHeight
+    };
+
+    this.setState({
+      bullets: this.state.bullets.concat(bullet),
+      bulletCounter: key + 1
+    });
+  }
+
+  _propelBullets() {
+    this.setState({
+      bullets: this.state.bullets.map(bullet => {
+        bullet.y += 2;
+        return bullet;
+      }).filter(bullet => bullet.y < this.props.height)
+    });
   }
 };
