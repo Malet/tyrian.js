5f4b951fb4bb3fabae44659611f8b332773d0341
Michael Malet <michael@rubypx.com>
2016-04-22 17:37:14 +0100

    Fix for parallax shift when spawning enemies/bullets

--- models/level.js
+++ models/level.js
@@ -1,6 +1,7 @@
 module.exports = {
   addEnemy: (state, enemy) => {
     enemy.key = state.enemySeq;
+    enemy.x -= (state.level.parallax * state.level.parallaxScale);
     state.enemies.push(enemy);
     state.enemySeq += 1;
     return state;
